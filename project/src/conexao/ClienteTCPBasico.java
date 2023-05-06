c7d0beacecf029b2e7cd008bea6df1b056420379
Bruno José <bruninhosese12@gmail.com>
2023-05-06 12:40:55 -0300

    conexão ao servidor

--- project/src/conexao/ClienteTCPBasico.java
+++ project/src/conexao/ClienteTCPBasico.java
@@ -1,22 +1,34 @@
 package conexao;
 
-import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.util.Date;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.sql.Date;
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class ClienteTCPBasico {
+
     public static void main(String[] args) {
         try {
-          Socket cliente = new Socket("Bruno", 12345);
-          ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-          Date data_atual = (Date)entrada.readObject();
-          JOptionPane.showMessageDialog(null,"Data recebida do servidor:" + data_atual.toString());
-          entrada.close();
-          System.out.println("Conexão encerrada");
+            Socket socket = new Socket("localhost", 12345); // Endereço IP e porta do servidor
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite uma mensagem: ");
+            String mensagem = scanner.nextLine();
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(mensagem); // Envia uma mensagem para o servidor
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = in.readLine(); // Recebe a mensagem do servidor
+
+            // Lógica para tratar a mensagem recebida do servidor
+        } catch (IOException e) {
+                 // Tratamento de exceção
         }
-        catch(Exception e) {
-          System.out.println("Erro: " + e.getMessage());
-        }
-      }
+    }
 }
