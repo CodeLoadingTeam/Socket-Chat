b6c8579eff5ef0a5a4a90befeb36f698964943fa
Bruno José <bruninhosese12@gmail.com>
2023-05-22 18:48:32 -0300

    dois clientes em unica execução

--- project/src/socket/Cliente.java
+++ project/src/socket/Cliente.java
@@ -7,10 +7,15 @@ public class Cliente {
 
     public static void main(String[] args) throws IOException {
 
-        Socket cliente = new Socket("localhost", 4000);
-        Tela tela = new Tela(cliente);
+        Socket cliente  = new Socket("localhost", 4000);
+        Socket cliente2 = new Socket("localhost", 4000);
+
+        Tela tela  = new Tela(cliente);
+        Tela tela2 = new Tela(cliente2);
         tela.inicializar();
+        tela2.inicializar();
 
         new ClienteThread(tela).start(); 
+        new ClienteThread(tela2).start(); 
     }
 }
